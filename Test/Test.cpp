b1f134300b56bb8c85f362a10950fe15121d92d6
Andrew <svt.andrey1631@gmail.com>
2019-11-09 19:23:07 +0200

    Версия 2

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -6,12 +6,12 @@ int main()
 	int num; int temp;
 	for (int i = 1; i <= 100; ++i)
 	{
-		cout << "i = " << i;
+		
 		temp = i; num = 0;
 		while (temp)
 		{
 			num = num * 10 + (temp % 10);
-			cout << "   num = " << num;
+			
 
 			temp /= 10; cout << "   temp = " << temp << endl;
 		}
@@ -19,4 +19,4 @@ int main()
 		system("pause");
 	} system("pause");
 }
-	
+	//
